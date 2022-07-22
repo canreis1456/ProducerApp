66a1d8e41437bccdd4925f9dbaa9a3f2c17a1eda
canreis1456 <c.kilic@ug.bilkent.edu.tr>
2022-07-22 11:07:16 +0300

    Repository, Service and Controller classes created

--- src/main/java/com/producer/producerapp/usecases/producer/Controller/ProducerController.java
+++ src/main/java/com/producer/producerapp/usecases/producer/Controller/ProducerController.java
@@ -1,4 +1,24 @@
 package com.producer.producerapp.usecases.producer.Controller;
 
-public class ProducerController {
+import com.producer.producerapp.usecases.common.dto.Response;
+import com.producer.producerapp.usecases.producer.Repository.dto.Entity;
+import com.producer.producerapp.usecases.producer.Service.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("ProducerController")
+@CrossOrigin(origins = "http://localhost:8080")
+public class ProducerController
+{
+    @Autowired
+    ProducerService producerService;
+
+    @PostMapping("producer")
+    public Response login(@RequestBody Entity entityInfo)
+    {
+        return producerService.produce(entityInfo);
+    }
 }
--- src/main/java/com/producer/producerapp/usecases/producer/Repository/ProducerRepository.java
+++ src/main/java/com/producer/producerapp/usecases/producer/Repository/ProducerRepository.java
@@ -1,4 +1,13 @@
 package com.producer.producerapp.usecases.producer.Repository;
 
-public class ProducerRepository {
+import com.producer.producerapp.usecases.producer.Repository.dto.Entity;
+import org.springframework.stereotype.Repository;
+@Repository
+public class ProducerRepository
+{
+    public int produce(Entity entityInfo) {
+        System.out.println(entityInfo.getUsername());
+        System.out.println(entityInfo.getPassword());
+        return 1;
+    }
 }
