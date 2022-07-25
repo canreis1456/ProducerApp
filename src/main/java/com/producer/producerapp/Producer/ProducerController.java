bf117e2572a3ca6993f2f4d8f5bcf42b3909231a
canreis1456 <c.kilic@ug.bilkent.edu.tr>
2022-07-25 14:25:14 +0300

    EntityGenerator and Controller classes modified

--- src/main/java/com/producer/producerapp/Producer/ProducerController.java
+++ src/main/java/com/producer/producerapp/Producer/ProducerController.java
@@ -1,5 +1,6 @@
 package com.producer.producerapp.Producer;
 
+import com.producer.producerapp.Model.EntityGenerator;
 import com.producer.producerapp.Model.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -7,28 +8,45 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Element;
 
-// Annotation
+import javax.persistence.EntityManager;
+import java.util.Map.Entry;
+
+
 @RestController
 @Configuration
 @EnableScheduling
-public class ProducerController {
+public class ProducerController extends EntityGenerator{
 
     // Autowiring Kafka Template
-    @Autowired KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
 
     private static final String TOPIC = "NewTopic";
 
+
     @Scheduled(fixedDelay = 1000)
     @GetMapping("/publish")
-    public String produce()
+    public void produce()
     {
+        String CurNameM = EntityGenerator.CurName;
+        String ForBuyM = EntityGenerator.ForexBuying;
+        String ForSellM = EntityGenerator.ForexSelling;
+        String BankBuyM = EntityGenerator.BanknoteBuying;
+        String BankSellM = EntityGenerator.BanknoteSelling;
+
+        System.out.println(CurNameM + " + " + ForBuyM + " + " + ForSellM + " + " + BankBuyM + " + " + BankSellM);
+
         Entity message = Entity.builder()
-                .currencyName("dollar").currencyValue(17.66).build();
+                .CurrencyName(EntityGenerator.CurrencyNameMethod(CurNameM))
+                .ForexBuying(EntityGenerator.ForexBuyingMethod(ForBuyM))
+                .ForexSelling(EntityGenerator.ForexSellingMethod(ForSellM))
+                .BanknoteBuying(EntityGenerator.BanknoteBuyingMethod(BankBuyM))
+                .BanknoteSelling(EntityGenerator.BanknoteSellingMethod(BankSellM))
+                .build();
 
-        // Sending the message
         kafkaTemplate.send(TOPIC, message.toString());
-
-        return "Published Successfully";
+        EntityGen();
     }
 }
